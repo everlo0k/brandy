465611d4bece6c06832b0c9dacc3936a0fd8e89c
everlook <everlook@126.com>
2018-05-21 23:01:18 +0800

    Initial commit

---
+++ src/main/java/com/bigears/brandy/BrandyApplication.java
@@ -0,0 +1,12 @@
+package com.bigears.brandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandyApplication.class, args);
+	}
+}
